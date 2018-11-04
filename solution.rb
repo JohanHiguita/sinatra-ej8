e783b8bdc061e4dd58b6c2fa0234d7d2c78d05d0
Johan Higuita <higuitajohan@gmail.com>
2018-11-04 08:26:50 -0500

    reto 8 sinatra, correción 2 usando gem device_detector

--- solution.rb
+++ solution.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
 # require 'rubygems'
 require "browser"
+require "device_detector"
 
 
 get '/' do
 	
-	browser = Browser.new(request.env['HTTP_USER_AGENT'], accept_language: "en-us")
-	# "#{browser.name}"
-	@browser_name=browser.name
+	#gem browser:
+	# browser = Browser.new(request.env['HTTP_USER_AGENT'], accept_language: "en-us")
+	# # "#{browser.name}"
+	# @browser_name=browser.name
+
+	#gem device_detector
+	client = DeviceDetector.new(request.env['HTTP_USER_AGENT'])
+	@browser_name=client.name # => 'Chrome'
 	erb :index
 
 end
